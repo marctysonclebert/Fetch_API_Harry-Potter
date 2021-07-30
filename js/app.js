c0694c787515706284260ccb1b37d9a924d1e09c
MarcTyson <codewithmarc@gmail.com>
2021-07-30 06:58:30 -0400

    Initialize the project

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const searchBar = document.querySelector(".search-bar");
+
+let charactersTable = [];
+
+//Events
+getCaracters();
+
+searchBar.addEventListener("keyup", e => {
+	let { value } = e.target;
+
+	value = value.toLowerCase();
+
+	let searchCharacters = charactersTable.filter(caracter => {
+		return (
+			caracter.name.toLowerCase().includes(value) ||
+			caracter.house.toLowerCase().includes(value)
+		);
+	});
+
+	displayCaracters(searchCharacters);
+});
+
+//Functions
+function displayCaracters(charactersArray) {
+	let caracterItems = charactersArray
+		.map(caracter => {
+			return `
+            <li class="caracter">
+            <div class="caracter-infos">
+                <h3 class="caracter-name">
+                    ${caracter.name}
+                </h3>
+                <p class="caracter-house">
+                    ${caracter.house}
+                </p>
+            </div>
+            <div class="caracter-image">
+                <img src="${caracter.image}" alt="${caracter.name}">
+            </div>
+        </li>
+            `;
+		})
+		.join("");
+
+	document.querySelector(".caracters").innerHTML = caracterItems;
+}
+
+async function getCaracters() {
+	try {
+		const response = await fetch("http://hp-api.herokuapp.com/api/characters");
+		charactersTable = await response.json();
+        displayCaracters(charactersTable);
+	} catch (error) {
+        console.log(error);
+	}
+}
